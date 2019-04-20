709d1f1d26c7ba5372af17d6eff079f09d44248d
Christian Bremer <christian.bremer@cstx.de>
2019-04-20 13:23:26 +0200

    Enums added

---
+++ src/main/java/org/bremersee/garmin/trip/v1/model/ext/TripTransportationMode.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bremersee.garmin.trip.v1.model.ext;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * The trip transportation mode.
+ *
+ * @author Christian Bremer
+ */
+@SuppressWarnings("unused")
+public enum TripTransportationMode {
+
+  /**
+   * Automotive transportation mode.
+   */
+  AUTOMOTIVE("Automotive");
+
+  private String value;
+
+  TripTransportationMode(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String toString() {
+    return value;
+  }
+
+  /**
+   * From value.
+   *
+   * @param value the value
+   * @return the trip transportation mode
+   */
+  @JsonCreator
+  public static TripTransportationMode fromValue(String value) {
+    for (TripTransportationMode e : TripTransportationMode.values()) {
+      if (e.value.equalsIgnoreCase(value)) {
+        return e;
+      }
+    }
+    return null;
+  }
+}
